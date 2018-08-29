5e966c14a990d08334239de64730f7cf3c920fe0
David Mitten <david@honestbrew.co.uk>
2018-08-29 15:09:25 +0100

    added in new triall

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -1,46 +1,90 @@
+// attempt 1
+// class Year {
+// 	constructor(year){
+// 		this.year = year;
+// 	}
+// 	isLeap() {
+// 			return isLeapYear(this.year)
+// 		}
+
+// 		isLeapYear(year){
+// 			var aLeap = year % 4 == 0;
+// 			if ( year % 100 !== 0 && year % 400 !== 0  && aLeap){
+
+// 				return aLeap;
+// 		}
+
+
+// 			}
+// 	}
+// }
+
+
+
+// export default Year;
+
+
+
+//attempt 2
+
+// class Year {
+// 	constructor(year){
+// 		this.year = year;
+// 	}
+// 	isLeap() {
+// 			return isLeapYear(this.year)
+// 		}
+
+// 		isLeapYear(year){
+// 				if(year % 4 === 0) return true;
+// 			} elseif(year )
+// 	}
+// }
+
+
+
+
+// export default Year;
+
+
+
+//help from mentor
+
+// // Actual solution
+// const isLeapYear = year => {
+//   console.log('@todo, determine if is leap year:', year);
+//   return false;
+// };
+
+// // And for unit tests
+// class Year {
+//   constructor(year) {
+//     this.year = year;
+//   }
+
+//   isLeap() {
+//     return isLeapYear(this.year);
+//   }
+// }
+
+// export default Year;
+
+// should work!
 class Year {
-	constructor(year){
+	constructor(year) {
 		this.year = year;
 	}
 	isLeap() {
-			return isLeapYear(this.year)
-		}
+		return isLeapYear(this.year)
+	}
 
-		isLeapYear(year){
-			var aLeap = year % 4 == 0;
-			if ( year % 100 !== 0 && year % 400 !== 0  && aLeap){
-				
-				return aLeap;
-		}
-		
+	isLeapYear(year) {
+		(year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
+		return year
 
-			}
 	}
 }
 
 
-
-export default Year;
-
-
-
-
-
-// Actual solution
-const isLeapYear = year => {
-  console.log('@todo, determine if is leap year:', year);
-  return false;
-};
-
-// And for unit tests
-class Year {
-  constructor(year) {
-    this.year = year;
-  }
-
-  isLeap() {
-    return isLeapYear(this.year);
-  }
-}
 
 export default Year;
